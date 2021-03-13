c711264942c952ad1349a933571a4dd22662220d
surya <surya568@gmail.com>
2021-03-13 17:36:55 +0530

    first commit

---
+++ Dollar.java
@@ -0,0 +1,12 @@
+/** Name: Manjusha
+** Description: Rupees to Dollar
+** Date: 12/03/2021
+**/
+class Dollar
+{
+public static void main(String args[])
+{
+double Rs=0.014;
+System.out.println(20*0.014+"USD");
+}
+}
